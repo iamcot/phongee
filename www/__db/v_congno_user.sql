6c5d2e1411156c71b272a0afea238bf6f0bf83ce
cot <t.truong@laudert.de>
2014-02-17 17:25:45 +0700

    edit money

--- www/__db/v_congno_user.sql
+++ www/__db/v_congno_user.sql
@@ -5,7 +5,7 @@ m.user_id,
 SUM((CASE WHEN (m.pgtype='nhap') THEN (m.pgamount*m.pgmoneyrate) ELSE 0 END)) sumnhap,
 SUM((CASE WHEN (m.pgtype='xuat') THEN (m.pgamount*m.pgmoneyrate) ELSE 0 END)) sumxuat
 FROM v_moneytransfer m
-WHERE m.pgdeleted=0 AND m.user_id > 0
+WHERE m.pgdeleted=0 AND m.user_id > 0 AND m.pginout_id = 0
 group by m.user_id
 
 //new
