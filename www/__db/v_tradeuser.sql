e5ddd9e15d09b8722e146cb01d5c44afe3967c53
cot <t.truong@laudert.de>
2014-02-19 18:13:08 +0700

    create trade user

--- www/__db/v_tradeuser.sql
+++ www/__db/v_tradeuser.sql
@@ -3,7 +3,10 @@ AS
 SELECT
 t.id tradeid,
 t.pgstore_id tradestore_id,
+s.pglong_name,
 u.*
 FROM pgtradeuser t
 LEFT JOIN pguser u
 ON u.id = t.pguser_id
+LEFT JOIN pgstore s
+ON s.id = t.pgstore_id
