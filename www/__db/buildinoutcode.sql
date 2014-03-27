611e7546dd0034b92b4c312d7c6bb8acc978691a
cot <t.truong@laudert.de>
2014-03-27 11:12:56 +0700

    change proc

--- www/__db/buildinoutcode.sql
+++ www/__db/buildinoutcode.sql
@@ -1,15 +1,14 @@
-DELIMITER $$
 DROP PROCEDURE IF EXISTS `buildinoutcode`$$
-CREATE PROCEDURE buildinoutcode (IN pgtype VARCHAR(5),OUT inoutcode INT )
-BEGIN
-DECLARE vcrrnum INT(11);
-IF pgtype != 'nhap' THEN
-SELECT xuat INTO vcrrnum FROM pginoutcode LIMIT 0,1;
-SET inoutcode = vcrrnum + 1;
-UPDATE pginoutcode SET xuat = inoutcode LIMIT 1;
-ELSE
-SELECT nhap INTO vcrrnum FROM pginoutcode LIMIT 0,1;
-SET inoutcode = vcrrnum + 1;
-UPDATE pginoutcode SET nhap = inoutcode LIMIT 1;
-END IF;
-END$$
+     CREATE PROCEDURE buildinoutcode (IN pgtype VARCHAR(5),OUT inoutcode INT )
+     BEGIN
+     DECLARE vcrrnum INT(11);
+     IF pgtype != 'nhap' THEN
+     SELECT xuat INTO vcrrnum FROM pginoutcode LIMIT 0,1;
+     SET inoutcode = vcrrnum + 1;
+     UPDATE pginoutcode SET xuat = inoutcode LIMIT 1;
+     ELSE
+     SELECT nhap INTO vcrrnum FROM pginoutcode LIMIT 0,1;
+     SET inoutcode = vcrrnum + 1;
+     UPDATE pginoutcode SET nhap = inoutcode LIMIT 1;
+     END IF;
+     END$$
